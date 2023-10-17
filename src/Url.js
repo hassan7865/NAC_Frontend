22a1d284f260c629769f5c313ccab4c836103a6c
hassan7865 <hassansiddiqui7865@gmail.com>
2023-10-18 00:06:54 +0500

    firstcommit

--- src/Url.js
+++ src/Url.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 const BASE_URL = "http://localhost:8800/api"
 const User = JSON.parse(localStorage.getItem("persist:root"))?.current
 const token = User && JSON.parse(User)?.token
-console.log(token)
 export const Req = axios.create({
     baseURL:BASE_URL,
     headers:{token:`Bearer ${token}`}
