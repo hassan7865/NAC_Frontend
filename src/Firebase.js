53b8c875373fcce6731c43f37b2d0d13d8e33965
hassan7865 <hassansiddiqui7865@gmail.com>
2023-10-22 23:01:17 +0500

    new

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,18 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDihF6g-q9qzAdQz-wC4mByFt3jLdd0SIw",
-  authDomain: "nedblogs-ae3d8.firebaseapp.com",
-  projectId: "nedblogs-ae3d8",
-  storageBucket: "nedblogs-ae3d8.appspot.com",
-  messagingSenderId: "178358875393",
-  appId: "1:178358875393:web:432e92e189587413cb5700"
+  apiKey: "AIzaSyCXUgP5665ztGX9hZn7ncqtz7eat-2J-f8",
+  authDomain: "nedamission.firebaseapp.com",
+  projectId: "nedamission",
+  storageBucket: "nedamission.appspot.com",
+  messagingSenderId: "301458115959",
+  appId: "1:301458115959:web:f38ef37f802dd9e0a4a572"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app
+export default app;
