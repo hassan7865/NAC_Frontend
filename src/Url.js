4e4f94fbb87acad2cccf4a3cb626e1cfb83e5c8c
hassan7865 <hassansiddiqui7865@gmail.com>
2023-10-22 15:04:07 +0500

    new

--- src/Url.js
+++ src/Url.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:8800/api";
+const BASE_URL = "https://fine-erin-giraffe-tie.cyclic.app/api";
 const userJSON = localStorage.getItem("persist:root");
 const User = userJSON && JSON.parse(userJSON)?.current
 export const token = User && JSON.parse(User)?.token 
