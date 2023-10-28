545c6e98c25ca0cf6710e5a40893eccfab5777a3
hassan7865 <hassansiddiqui7865@gmail.com>
2023-10-29 02:35:59 +0500

    new

--- Pages/SIngleBlog.jsx
+++ Pages/SIngleBlog.jsx
@@ -6,38 +6,40 @@ import { Req } from "../src/Url";
 import Loader from "../Loader/loader";
 import './blog.css'
 export default function SingleBlog() {
-    const [blog,setblog] = useState(null)
+    const [blog, setblog] = useState(null)
     const location = useLocation()
     const id = location.pathname.split("/")[3]
     const type = location.pathname.split("/")[2]
     const title = location.pathname.split("/")[4]
 
     var decodedString = decodeURIComponent(title);
-    const getBlog = async()=>{
+    const getBlog = async () => {
         await Req.get(`/blog/${type}/${id}/${decodedString}`)
-        .then((res)=>{
-            if(res.status === 200){
-                setblog(res.data)
-            }
-        })
+            .then((res) => {
+                if (res.status === 200) {
+                    setblog(res.data)
+                }
+            })
     }
-    useEffect(()=>{
+    useEffect(() => {
         getBlog()
-    },[])
+    }, [])
     let html;
-    if(blog){
+    if (blog) {
         html = blog.content
     }
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
     return (
         <>
-        {blog ? 
-        <Container maxWidth="xl" sx={{ marginTop: isSmallScreen ? "80px" : "100px",paddingX:"10px" }}>
-            <Box>
-                {blog && parse(html)}
-            </Box>
-        </Container>:<Loader/>}
+            {blog ?
+                <Container maxWidth="xl" sx={{ marginTop: isSmallScreen ? "80px" : "100px", paddingX: "10px",display:"flex",flexDirection:"column",alignItems:"center" }}>
+                    <Box>
+                        <div className="Html">
+                            {blog && parse(html)}
+                        </div>
+                    </Box>
+                </Container> : <Loader />}
         </>
     )
 }
