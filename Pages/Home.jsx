54ce242fde6e5f6448b9fbd796f23d34c2948d4b
Hassan Siddiqui <hassansiddiqui7865@gmail.com>
2025-06-15 16:59:23 +0500

    Changes Done

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -1,17 +1,29 @@
 import { useEffect } from "react";
-import Navbar from "../Components/Navbar";
 import Overview from "../Components/Overview";
 import { Req } from "../src/Url";
+
 export default function Home() {
-    useEffect(()=>{
-         const track = async () => {
-              await Req.post("/track-visit")
-            }
-            track();
-    },[])
-    return (
-       <div >
-        <Overview/>
-       </div>
-    )
-};
+  useEffect(() => {
+    const track = async () => {
+      const payload = {
+        referrer: document.referrer || null,
+        location: window.location.href,
+        userAgent: navigator.userAgent,
+      };
+
+      try {
+        await Req.post("/track-visit", payload);
+      } catch (err) {
+        console.error("Tracking failed", err);
+      }
+    };
+
+    track();
+  }, []);
+
+  return (
+    <div>
+      <Overview />
+    </div>
+  );
+}
