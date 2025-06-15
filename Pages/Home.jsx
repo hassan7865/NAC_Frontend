a4d9900589e9911e0463ffbbd99c5b6e6fe3987d
Hassan Siddiqui <hassansiddiqui7865@gmail.com>
2025-06-15 16:54:36 +0500

    Changes Done

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import Navbar from "../Components/Navbar";
 import Overview from "../Components/Overview";
+import { Req } from "../src/Url";
 export default function Home() {
     useEffect(()=>{
          const track = async () => {
-              await Req.get("/track-visit")
+              await Req.post("/track-visit")
             }
             track();
     },[])
